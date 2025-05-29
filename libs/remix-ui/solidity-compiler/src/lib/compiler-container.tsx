d85ab882bd37148a5e587fd73993b4b8ef046e1b
Anukul Pandey <anukul.030601@gmail.com>
2025-05-29 17:26:41 +0530

    feat: version sorting

--- libs/remix-ui/solidity-compiler/src/lib/compiler-container.tsx
+++ libs/remix-ui/solidity-compiler/src/lib/compiler-container.tsx
@@ -380,7 +380,6 @@ export const CompilerContainer = (props: CompilerContainerProps) => {
         const releasedVersions = solJsonBinData.selectorList.filter((obj) => !obj.prerelease).map((obj) => obj.version)
         const allVersions = solJsonBinData.selectorList.map((obj) => _retrieveVersion(obj.version))
 
-        console.log("allVersions===",allVersions)
         const currentCompilerName = _retrieveVersion(state.selectedVersion)
         // contains only numbers part, for example '0.4.22'
         const pureVersion = _retrieveVersion()
--- libs/remix-ui/solidity-compiler/src/lib/components/compiler-dropdown.tsx
+++ libs/remix-ui/solidity-compiler/src/lib/components/compiler-dropdown.tsx
@@ -1,7 +1,5 @@
 import { appPlatformTypes, platformContext, onLineContext } from '@remix-ui/app';
-
-;
-import React, { useEffect, useState, useRef, useReducer, useContext } from 'react' // eslint-disable-line
+import React, { useContext } from 'react' // eslint-disable-line
 import { Dropdown } from 'react-bootstrap';
 import { CompilerMenu, CompilerMenuToggle } from './compiler-menu';
 
@@ -21,10 +19,34 @@ interface compilerDropdownProps {
   onlyDownloaded: boolean
 }
 
+const isVersionLessThan = (v: string, target: string) => {
+  try {
+    const clean = (ver: string) => ver.replace(/^v/, '').split('+')[0].split('.').map(Number);
+    const [v1, v2, v3] = clean(v);
+    const [t1, t2, t3] = clean(target);
+    if (v1 !== t1) return v1 < t1;
+    if (v2 !== t2) return v2 < t2;
+    return v3 < t3;
+  } catch (e) {
+    return false;
+  }
+}
+
 export const CompilerDropdown = (props: compilerDropdownProps) => {
   const online = useContext(onLineContext)
   const platform = useContext(platformContext)
-  const { customVersions, selectedVersion, defaultVersion, allversions, handleLoadVersion, _shouldBeAdded, onlyDownloaded } = props
+  let { customVersions, selectedVersion, defaultVersion, allversions, handleLoadVersion, _shouldBeAdded, onlyDownloaded } = props
+
+  console.log("defaultVersion===",defaultVersion);
+
+  // Force default version if none is selected
+  if (!selectedVersion && defaultVersion) {
+    selectedVersion = defaultVersion
+  }
+
+  // Filter only versions less than 0.8.11
+  const filteredVersions = allversions.filter(build => isVersionLessThan(build.longVersion, '0.8.12'))
+
   return (
     <Dropdown id="versionSelector" data-id="versionSelector">
       <Dropdown.Toggle as={CompilerMenuToggle} id="dropdown-custom-components" className="btn btn-light btn-block w-100 d-inline-block border border-dark form-control" icon={null}>
@@ -33,9 +55,8 @@ export const CompilerDropdown = (props: compilerDropdownProps) => {
             {customVersions.map((url, i) => {
               if (selectedVersion === url) return (<span data-id="selectedVersion" key={i}>custom</span>)
             })}
-            {allversions.map((build, i) => {
-
-              if ((selectedVersion || defaultVersion) === build.path) {
+            {filteredVersions.map((build, i) => {
+              if (selectedVersion === build.path) {
                 return (<span data-id="selectedVersion" key={i}>{build.longVersion}</span>)
               }
             })}
@@ -44,37 +65,33 @@ export const CompilerDropdown = (props: compilerDropdownProps) => {
       </Dropdown.Toggle>
 
       <Dropdown.Menu as={CompilerMenu} className="w-100 custom-dropdown-items overflow-hidden" data-id="custom-dropdown-items">
-        {allversions.length <= 0 && (
-          <Dropdown.Item
-            key={`default`}
-            data-id='builtin'
-            onClick={() => {}}
-          >
-            <div className='d-flex w-100 justify-content-between'>
-              {selectedVersion === defaultVersion ? <span className='fas fa-check text-success mr-2'></span> : null}
-              <div style={{ flexGrow: 1, overflow: 'hidden' }}>
-                <div className="text-truncate">
-                  {defaultVersion}
+        {filteredVersions.length <= 0 && (
+          <>
+            <Dropdown.Item
+              key={`default`}
+              data-id='builtin'
+              onClick={() => {}}
+            >
+              <div className='d-flex w-100 justify-content-between'>
+                {selectedVersion === defaultVersion ? <span className='fas fa-check text-success mr-2'></span> : null}
+                <div style={{ flexGrow: 1, overflow: 'hidden' }}>
+                  <div className="text-truncate">{defaultVersion}</div>
                 </div>
               </div>
-            </div>
-          </Dropdown.Item>
-        )}
-        {allversions.length <= 0 && (
-          <Dropdown.Item
-            key={`builtin`}
-            data-id='builtin'
-            onClick={() => {}}
-          >
-            <div className='d-flex w-100 justify-content-between'>
-              {selectedVersion === "builtin" ? <span className='fas fa-check text-success mr-2'></span> : null}
-              <div style={{ flexGrow: 1, overflow: 'hidden' }}>
-                <div className="text-truncate">
-                  builtin
+            </Dropdown.Item>
+            <Dropdown.Item
+              key={`builtin`}
+              data-id='builtin'
+              onClick={() => {}}
+            >
+              <div className='d-flex w-100 justify-content-between'>
+                {selectedVersion === "builtin" ? <span className='fas fa-check text-success mr-2'></span> : null}
+                <div style={{ flexGrow: 1, overflow: 'hidden' }}>
+                  <div className="text-truncate">builtin</div>
                 </div>
               </div>
-            </div>
-          </Dropdown.Item>
+            </Dropdown.Item>
+          </>
         )}
         {customVersions.map((url, i) => (
           <Dropdown.Item
@@ -85,14 +102,12 @@ export const CompilerDropdown = (props: compilerDropdownProps) => {
             <div className='d-flex w-100 justify-content-between'>
               {selectedVersion === url ? <span className='fas fa-check text-success mr-2'></span> : null}
               <div style={{ flexGrow: 1, overflow: 'hidden' }}>
-                <div className="text-truncate">
-                  custom: {url}
-                </div>
+                <div className="text-truncate">custom: {url}</div>
               </div>
             </div>
           </Dropdown.Item>
         ))}
-        {allversions.map((build, i) => {
+        {filteredVersions.map((build, i) => {
           if (onlyDownloaded && !build.isDownloaded) return null
           return _shouldBeAdded(build.longVersion) ? (
             <Dropdown.Item
@@ -103,11 +118,12 @@ export const CompilerDropdown = (props: compilerDropdownProps) => {
               <div className='d-flex w-100 justify-content-between'>
                 {selectedVersion === build.path ? <span className='fas fa-check text-success mr-2'></span> : null}
                 <div style={{ flexGrow: 1, overflow: 'hidden' }}>
-                  <div className="text-truncate">
-                    {build.longVersion}
-                  </div>
+                  <div className="text-truncate">{build.longVersion}</div>
                 </div>
-                {platform == appPlatformTypes.desktop ? (build.isDownloaded ? <div className='fas fa-arrow-circle-down text-success ml-auto'></div> : <div className='far fa-arrow-circle-down'></div>) : null}
+                {platform === appPlatformTypes.desktop ? (
+                  build.isDownloaded ? <div className='fas fa-arrow-circle-down text-success ml-auto'></div>
+                  : <div className='far fa-arrow-circle-down'></div>
+                ) : null}
               </div>
             </Dropdown.Item>
           ) : null
